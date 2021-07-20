c6dd280cae6c0ced370013ec942121e802911882
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2021-07-20 20:10:43 +0900

    無限スクロール,followPosts実装

--- src/components/posts/PostIndexItem/PostsNew.tsx
+++ src/components/posts/PostIndexItem/PostsNew.tsx
@@ -67,30 +67,3 @@ const PostsNew = () => {
   )
 }
 export default PostsNew
-
-// import { VFC } from 'react';
-// import { useAppSelector } from '../../../app/hooks';
-// import { selectQueryGenre, selectQueryPlace } from '../../../slices/postSlice';
-// import PostsItemNew from './PostsNewItem/PostsItemNew';
-// import PostsItemNewGenre from './PostsNewItem/PostsItemNewGenre';
-
-// const PostsNew:VFC = () => {
-//   const queryGenre = useAppSelector(selectQueryGenre)
-//   const queryPlace = useAppSelector(selectQueryPlace)
-
-//   return (
-//     <div>
-//       { queryPlace && queryGenre ?
-//       // <PostsAllNew/>
-//       <h2>どっちも</h2>
-//       : queryPlace ?
-//       <PostsItemNew/>
-//       : queryGenre ?
-//       <PostsItemNewGenre/>
-//       : <PostsItemNew/>
-//       }
-//     </div>
-//   )
-// }
-
-// export default PostsNew
--- src/hooks/reactQuery/useQueryInfinitePostsFollow.ts
+++ src/hooks/reactQuery/useQueryInfinitePostsFollow.ts
@@ -3,20 +3,20 @@ import { useInfiniteQuery } from 'react-query';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { incrementQueryPage, selectPage } from '../../slices/postSlice';
 
-  export const useQueryInfinitePostsNew = (genre:string | null,place:string | null) => {
+  export const useQueryInfinitePostsFollow = (userId:string | null, genre:string | null,place:string | null) => {
     const dispatch = useAppDispatch()
     const page = useAppSelector(selectPage)
     
-    const getInfinitePostsNew = async ({pageParam = 0}) => {
+    const getInfinitePostsFollow = async ({pageParam = 0}) => {
 
-      const { data } = await axios.get(`${process.env.REACT_APP_REST_URL}/posts/new/${genre}/${place}/page/` + pageParam)
+      const { data } = await axios.get(`${process.env.REACT_APP_REST_URL}/posts/follow/${userId}/${genre}/${place}/page/` + pageParam)
         // ジャンルを配列に変換
         data.posts.map((v:any) => v.genre = v.genre.split(','))
         dispatch(incrementQueryPage())    
         return data
     }
     
-      return useInfiniteQuery('postsInfiniteNew', getInfinitePostsNew, 
+      return useInfiniteQuery('postsInfiniteFollow', getInfinitePostsFollow, 
       {
         getNextPageParam: (lastPage) =>
           lastPage.posts.length !== 0 ? page : false,
